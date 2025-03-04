29a55b1ef3881914343d51adcbb349897aea9e82
Israël TODOME <tdonanon@outlook.fr>
2025-03-03 21:27:19 -0500

    Ajoutez des fichiers projet.

---
+++ EXERCICESXMLREADER/Personne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICESXMLREADER
+{
+    public class Personne
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime DateDeNaissance { get; set; }
+        public int Taille { get; set; }
+
+
+    }
+}
---
+++ EXERCICESXMLREADER/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace EXERCICESXMLREADER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = XmlReader.Create("fichier.xml");
+            var personne = new Personne();
+
+            reader.MoveToContent();
+            reader.ReadStartElement("Personne");
+
+            reader.ReadStartElement("Nom");
+            personne.Nom = reader.ReadContentAsString();
+            reader.ReadEndElement();
+
+            reader.ReadStartElement("Prenom");
+            personne.Prenom = reader.ReadContentAsString();
+            reader.ReadEndElement();
+
+            reader.ReadStartElement("DateDeNaissance");
+            personne.DateDeNaissance = reader.ReadContentAsDateTime();
+            reader.ReadEndElement();
+
+            reader.ReadStartElement("Taille");
+            personne.Taille = reader.ReadContentAsInt();
+            reader.ReadEndElement();
+
+            reader.ReadEndElement();
+
+            System.Console.WriteLine($"{personne.Prenom} {personne.Nom} né le {personne.DateDeNaissance:dd/MM/yyyy} mesure {personne.Taille}cm");
+        }
+    }
+}
